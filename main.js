2ec9f3d4d01b5e9649deb072463d7c5591dfdaed
asef0024 <asef0024@stud.kea.dk>
2022-05-12 09:46:29 +0200

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
 "use strict";
 import "./sass/style.scss";
-
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
